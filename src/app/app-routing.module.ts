84a510688254687394c42d03ba80411e95bf10f0
Your Name <you@example.com>
2019-08-28 03:00:16 +0200

    commit /e/EkraaAngular/ekraa.proj

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BaseComponent} from './base/base.component';
+import { ProductsComponent } from './widgets/products/products.component';
 
 const routes: Routes = [
   { path: '' , component: BaseComponent },
-  { path: 'home' , component: BaseComponent },/*
-  { path: '/home', component: CrisisListComponent },
-  { path: '**', component: PageNotFoundComponent } */];
+  { path: 'home' , component: BaseComponent },
+  // { path: '/home', component: CrisisListComponent },
+    // { path: '**', component: PageNotFoundComponent } */ 
+  {path: 'products' , component:ProductsComponent}
+  ];
+
+export default routes ;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/widgets/widgets.module.ts
+++ src/app/widgets/widgets.module.ts
@@ -4,7 +4,6 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { OurProductsComponent } from './our-products/our-products.component';
 import { AboutComponent } from './about/about.component';
 import { DescriptionComponent } from './description/description.component';
-import { FooterComponent } from './footer/footer.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { LessonsComponent } from './lessons/lessons.component';
 import { ProductsComponent } from './products/products.component';
